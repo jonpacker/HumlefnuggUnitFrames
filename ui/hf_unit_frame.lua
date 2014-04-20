dc27ba65db80603fb73f97f0774045caec1b6f10
Jon Packer <contact@jonpacker.com>
2014-04-20 18:00:04 +0200

    bar proportions

--- ui/hf_unit_frame.lua
+++ ui/hf_unit_frame.lua
@@ -17,27 +17,31 @@ local render = function(uf, parent)
 
   uf.container = container
 
+  local barWidth = uf.opts.width - uf.opts.padding * 2
+  local barVerticalSpace = uf.opts.height - uf.opts.padding * 2
+
   uf.healthBar = HFGrowbar:create(container, {
-    fgColour = { 255/255, 46/255, 0, 1 };
-    bgColour = { 54/255, 54/255, 54/255, 1 };
-    width = uf.opts.width;
-    height = uf.opts.height / 3;
+    fgColour = { 227/255, 94/255, 51/255, 1 };
+    bgColour = { 30/255, 30/255, 30/255, 1 };
+    width = barWidth;
+    height = barVerticalSpace * 0.5 - uf.opts.padding;
   })
   uf.magickaBar = HFGrowbar:create(container, {
     fgColour = { 38/255, 94/255, 176/255, 1 };
-    bgColour = { 54/255, 54/255, 54/255, 1 };
-    width = uf.opts.width;
-    height = uf.opts.height / 3;
+    bgColour = { 30/255, 30/255, 30/255, 1 };
+    width = barWidth;
+    height = barVerticalSpace * 0.25 - uf.opts.padding;
   })
   uf.staminaBar = HFGrowbar:create(container, {
     fgColour = { 84/255, 189/255, 2/255, 1 };
-    bgColour = { 54/255, 54/255, 54/255, 1 };
-    width = uf.opts.width;
-    height = uf.opts.height / 3;
+    bgColour = { 30/255, 30/255, 30/255, 1 };
+    width = barWidth;
+    height = barVerticalSpace * 0.25 - uf.opts.padding;
   })
 
-  uf.magickaBar.container:SetAnchor(TOPLEFT, uf.healthBar.container, BOTTOMLEFT, 0, 0)
-  uf.staminaBar.container:SetAnchor(TOPLEFT, uf.magickaBar.container, BOTTOMLEFT, 0, 0)
+  uf.healthBar.container:SetAnchor(TOPLEFT, uf.container, TOPLEFT, uf.opts.padding, uf.opts.padding)
+  uf.magickaBar.container:SetAnchor(TOPLEFT, uf.healthBar.container, BOTTOMLEFT, 0, uf.opts.padding)
+  uf.staminaBar.container:SetAnchor(TOPLEFT, uf.magickaBar.container, BOTTOMLEFT, 0, uf.opts.padding)
 end
 
 local listen = function(uf)
@@ -57,6 +61,7 @@ end
 local defaults = {
   height = 80;
   width = 360;
+  padding = 2;
 };
 defaults.__index = defaults;
 
