92b8e15bebabff292509102d002da65de6c60014
Jon Packer <contact@jonpacker.com>
2014-04-20 17:08:27 +0200

    more subtle glow

--- ui/hf_growbar.lua
+++ ui/hf_growbar.lua
@@ -31,19 +31,19 @@ end
 local animateGlowOnChange = function(bar)
   local timeline = ANIMATION_MANAGER:CreateTimeline()
   timeline:InsertCallback(function()
-    bar.glow:SetAlpha(1)
+    bar.glow:SetAlpha(bar.opts.glowMaxAlpha)
   end, 0)
 
   local disappear = timeline:InsertAnimation(ANIMATION_ALPHA, bar.glow, bar.opts.glowTime)
   disappear:SetDuration(bar.opts.glowTime / 2)
   disappear:SetEasingFunction(bar.opts.glowEasing)
-  disappear:SetAlphaValues(1, 0)
+  disappear:SetAlphaValues(bar.opts.glowMaxAlpha, 0)
 
   bar:on("update", function(value)
     if value == bar.value then return end
     timeline:Stop()
-    local widthDiff = math.abs(value - bar.value)
-    bar.glow:SetWidth(widthDiff * bar.opts.width)
+    widthDiff = value - bar.value
+    bar.glow:SetWidth(math.abs(widthDiff) * bar.opts.width)
     timeline:PlayFromStart()
   end)
 end
@@ -62,15 +62,15 @@ local render = function(bar, parent)
   fillBar:SetSimpleAnchorParent(0, 0)
 
   glow:SetDimensions(0, bar.opts.height)
-  glow:SetColor(1, 1, 1, 1);
+  glow:SetColor(1, 1, 1, bar.opts.glowMaxAlpha);
   glow:SetAnchor(TOPRIGHT, fillBar, TOPRIGHT, 0, 0)
 
   bar.container = container
   bar.bar = fillBar
   bar.glow = glow
 
-  animateGlowOnChange(bar)
   animateWidthChange(bar)
+  animateGlowOnChange(bar)
 end
 
 -- Available options:
@@ -96,6 +96,7 @@ function HFGrowbar:create(parent, opts)
   if opts.easing == nil then opts.changeEasing = ZO_EaseOutQuintic end
   if opts.glowTime == nil then opts.glowTime = 500 end
   if opts.glowEasing == nil then opts.glowEasing = ZO_EaseInQuintic end
+  if opts.glowMaxAlpha == nil then opts.glowMaxAlpha = 0.8 end
   if opts.height == nil then opts.height = 30 end
   if opts.width == nil then opts.width = 300 end
 
