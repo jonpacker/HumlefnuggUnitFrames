7c4df9e8358cd8e317373a1a44c1ba5812792fb8
Jon Packer <contact@jonpacker.com>
2014-04-20 14:48:53 +0200

    unit eventsource

---
+++ events/hf_unit.lua
@@ -0,0 +1,34 @@
+local delegate = EventEmitter:new()
+local delegateGlobalEventTo = function(globalEvent, localEvent)
+  EVENT_MANAGER:RegisterForEvent("HFUF", globalEvent, function(...)
+    delegate:emit(localEvent, ...)
+  end)
+end
+
+delegateGlobalEventTo(EVENT_POWER_UPDATE, "power-update")
+
+local eventSources = {}
+
+function HFUnitEventSource(unit)
+  if eventSources[unit] != nil then return eventSources[unit] end
+
+  local es = eventSources[unit] = EventEmitter:new()
+
+  delegate:on("power-update", function(eventCode, eventUnit, powerIndex, powerType, powerValue, powerMax, powerEffectiveMax)
+    if unit ~= eventUnit then return end
+  
+    local eventName = nil
+    if powerType == POWERTYPE_HEALTH then
+      eventName = "health-update"
+    elseif powerType == POWERTYPE_HEALTH then
+      eventName = "magicka-update"
+    elseif powerType == POWERTYPE_STAMINA then
+      eventName = "stamina-update"
+    end
+    
+    if eventName ~= nil then
+      es:emit(eventName, powerValue, powerMax, powerEffectiveMax)
+    end
+  end)
+end
+
