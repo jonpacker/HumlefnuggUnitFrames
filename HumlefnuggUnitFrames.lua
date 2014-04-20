5b741460055a04be3aca90e889ac6c1ea542383e
Jon Packer <contact@jonpacker.com>
2014-04-20 11:40:46 +0200

    boilerplate

---
+++ HumlefnuggUnitFrames.lua
@@ -0,0 +1,19 @@
+local LAM = LibStub:GetLibrary("LibAddonMenu-1.0")
+
+HFUF = {
+  defaults = {};
+}
+
+function HFUF.init(eventCode, addOnName)
+  if addOnName ~= "HumlefnuggUnitFrames" then return end
+
+  HFXB.settings = ZO_SavedVars:New("HFUFSettings", 2, nil, HFUF.defaults)
+
+  -- config menu
+  panel = LAM:CreateControlPanel("HFUFConfig", "Humlefnugg Unit Frames")
+
+  HFXB.updateSettings()
+end
+
+
+EVENT_MANAGER:RegisterForEvent("HFUF", EVENT_ADD_ON_LOADED, HFXB.init)
